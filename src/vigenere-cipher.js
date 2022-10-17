1e44b48d6676bd7dcec604ecd63816dafc900585
Angelica-zb <angelika.zaborovskaya@gmail.com>
2022-10-18 00:26:34 +0300

    fix

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(x) {
+        this.x = x
+    }
+
+
+    encrypt(text, key) {
+        if (!text || !key) {
+            throw new Error('Incorrect arguments!')
+        }
+        let message = (String(this.x) === 'false') ? text.split('').reverse().join('') : text;
+        let space = [];
+        for (let i = 0; i < message.length; i++) {
+            if (message[i] === ' ') {
+                space.push(i)
+            }
+        }
+        let str = message.split(' ').join('');
+        let t = str.length / key.length
+        let rep = Math.ceil(t)
+        let keyStr = key.repeat(rep).substring(0, str.length)
+        let str2 = '';
+        for (let i = 0; i < str.length; i++) {
+            if (str[i].toUpperCase().charCodeAt(0) <= 90 && str[i].toUpperCase().charCodeAt(0) >= 65) {
+                let codeL = str[i].toUpperCase().charCodeAt(0);
+                let codeK = keyStr[i].toUpperCase().charCodeAt(0);
+                if (codeL + (codeK - 65) > 90) {
+                    str2 += String.fromCharCode(codeL + codeK - 91)
+                } else {
+                    str2 += String.fromCharCode(codeL + codeK - 65)
+                }
+            } else {
+                str2 += str[i];
+            }
+        }
+        let arr = str2.split('')
+
+        for (let i = 0; i < space.length; i++) {
+            arr.splice(space[i], 0, ' ')
+        }
+        let result = arr.join('')
+        return result;
+    }
+
+
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) {
+            throw new Error('Incorrect arguments!')
+        }
+        let message = (String(this.x) === 'false') ? encryptedMessage.split('').reverse().join('') : encryptedMessage;
+        let space = [];
+        for (let i = 0; i < message.length; i++) {
+            if (message[i] === ' ') {
+                space.push(i)
+            }
+        }
+        let str = message.split(' ').join('');
+        let t = str.length / key.length
+        let rep = Math.ceil(t)
+        let keyStr = key.repeat(rep).substring(0, str.length)
+        let str2 = '';
+        for (let i = 0; i < str.length; i++) {
+            if (str[i].toUpperCase().charCodeAt(0) <= 90 && str[i].toUpperCase().charCodeAt(0) >= 65) {
+                let codeL = str[i].toUpperCase().charCodeAt(0);
+                let codeK = keyStr[i].toUpperCase().charCodeAt(0);
+                if (codeK <= codeL) {
+                    str2 += String.fromCharCode(65 - (codeK - codeL))
+                } else {
+                    str2 += String.fromCharCode(91 - (codeK - codeL))
+                }
+            } else {
+                str2 += str[i];
+            }
+        }
+        let arr = str2.split('')
+        for (let i = 0; i < space.length; i++) {
+            arr.splice(space[i], 0, ' ')
+        }
+        let result = arr.join('')
+        return result;
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
